f2747d5894939cec155492798d33851ccca3fcb7
Roman <brysommer1@gmail.com>
2023-12-20 16:51:13 +0200

    finished

--- anketa.js
+++ anketa.js
@@ -9,15 +9,6 @@ export const anketaListiner = async () => {
     bot.on('message', async (msg) => {
         let chatId = msg.chat.id;
         const text = msg.text; 
-        switch (msg.text) {
-            case '/start':
-                break;
-            case '🚙 Підібрати авто':
-                break;
-            case '📞 Звʼяжіться зі мною':
-                bot.sendMessage(chatId, phrases.callback, keyboards.sendContact);
-                break; 
-        }
         if (msg.contact) {
             customerPhone = msg.contact.phone_number;
             customerName = msg.contact.first_name;
